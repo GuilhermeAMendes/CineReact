55f7081e5a642c9ca8fa505189a40fee2aefdac9
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-01-22 14:39:28 -0300

    feat: create page of favorites

--- src/Pages/Favorite/index.js
+++ src/Pages/Favorite/index.js
@@ -1,18 +1,31 @@
+import { Link } from "react-router-dom";
 import useFetchFavorites from "../../hooks/useFetchFavorites";
+import "./styles.css";
 
 export default function Favorite() {
-  const favorites = JSON.parse(localStorage.getItem("favoriteMovies")) || [];
+  let favorites = JSON.parse(localStorage.getItem("favoriteMovies")) || [];
 
   const { favoritesInformation } = useFetchFavorites(favorites);
 
+  const handleExcludeFavoriteMovie = (id) => {
+    favorites = favorites.filter((fav) => fav != id);
+    localStorage.setItem("favoriteMovies", JSON.stringify(favorites));
+  };
+
   return (
-    <div>
+    <div className="my-movies">
       <h1>Meus filmes favoritos</h1>
       {favoritesInformation.length > 0 ? (
         <ul>
           {favoritesInformation.map((movie) => (
             <li key={movie.id}>
-              <h3>{movie.title}</h3>
+              <span>{movie.title}</span>
+              <div>
+                <Link to={`/Film/${movie.id}`}>Ver detalhes</Link>
+                <button onClick={() => handleExcludeFavoriteMovie(movie.id)}>
+                  Excluir
+                </button>
+              </div>
             </li>
           ))}
         </ul>
